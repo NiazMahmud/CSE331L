fd6abab6af7b8b10c5655d006029178056bce8d8
NiazMahmud <s.m.mahmud@northsouth.edu>
2021-03-18 10:06:28 +0600

    Lab Assessment-1

---
+++ Lab Assessment-1/1713079042.asm
@@ -0,0 +1,37 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+include 'emu8086.inc'
+org 100h
+
+lea si, arr
+
+mov cx, 5 
+
+
+mov ah,9
+mov dx,offset msg1
+
+int 21h
+
+take_input:
+mov ah,1
+int 21h
+sub al,48
+mov [si],al
+inc si
+loop take_input 
+
+ 
+print 'Completed'
+
+ret 
+
+arr db 0
+msg1 db "input; $  "
+
+
+
+
+
